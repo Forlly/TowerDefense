59a89417cdacbdbc0922c92471a035f37df8a5cc
DESKTOP-ESQGB1I\remyp <forrrllly@gmail.com>
2022-10-05 15:18:20 +0300

    Change UI towers and environment

--- Assets/Scripts/Towers/SimpleTower.cs
+++ Assets/Scripts/Towers/SimpleTower.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleTowerBuilding : ITower
+public class SimpleTower : ITower
 {
     
     public override IEnumerator StartAttack()
